17340dfb28635ff7052f02808582f281d83d52bc
Lucas Félix <lmateus8673@gmail.com>
2023-07-19 17:27:22 -0300

    Adicionando conteudo da noticia

--- v1/api.js
+++ v1/api.js
@@ -6,12 +6,14 @@ const knex = require('../Database/connection')
 
 router.get(routerbase + '/noticias', async (req, res) => {
     await knex.raw(`
-        SELECT convert_from(images, 'UTF8') as file, titulo, descricao, id FROM tb_noticias
+        SELECT convert_from(images, 'UTF8') as file, titulo, descricao, id, conteudo FROM tb_noticias ORDER BY id DESC LIMIT 5
     `).then( resp => {
         res.json(
             {
+                id: resp.rows[0].id,
                 titulo: resp.rows[0].titulo,
                 descricao: resp.rows[0].descricao,
+                conteudo: resp.rows[0].conteudo,
                 image: resp.rows[0].file
             }
         )
@@ -23,27 +25,31 @@ router.get(routerbase + '/noticia/:id', async (req, res) => {
     var id = req.params
 
     await knex.raw(`
-    SELECT convert_from(images, 'UTF8') as file, titulo, descricao, id FROM tb_noticias WHERE id = ${id.id}
+    SELECT convert_from(images, 'UTF8') as file, titulo, descricao, id, conteudo FROM tb_noticias WHERE id = ${id.id}
     `).then( resp => {
         res.json(
             {
+                id: resp.rows[0].id,
                 titulo: resp.rows[0].titulo,
                 descricao: resp.rows[0].descricao,
+                conteudo: resp.rows[0].conteudo,
                 image: resp.rows[0].file
             }
         )
     })
-    .catch(e => res.status(404).json({msg: 'not found'}))
+    .catch(e => res.status(404).json({msg: 'not found news with id ' + id.id}))
 })
 
 router.post(routerbase + '/add/noticia', async (req, res) => {
-    var {titulo, descricao} = req.body
+    var {titulo, descricao, conteudo} = req.body
     let matches = req.body.base64image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/) || req.body.base64image.match(/^data:@([A-Za-z-+\/]+);base64,(.+)$/);
     let response = {}
 
     response.type = matches[1]
     response.data = new Buffer.from(matches[2], 'base64');
-    await knex.raw(`INSERT INTO tb_noticias (titulo, images, descricao) VALUES ('${titulo}', '${matches[2]}', '${descricao}')`).then(() => {res.json({msg: "success"})})
+    await knex.raw(`INSERT INTO tb_noticias (titulo, images, descricao, conteudo) VALUES ('${titulo}', '${matches[2]}', '${descricao}', '${conteudo}')`)
+    .then(() => {res.json({msg: "success"})})
+    .catch(() => res.status(401).json({msg: 'Unhauthorized'}))
 })
 
 module.exports = router
